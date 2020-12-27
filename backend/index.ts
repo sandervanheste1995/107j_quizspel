103663865fec66220529b6591c586d94bfa5493b
Sander Vanheste <sandrvanheste@gmail.com>
2020-12-27 01:27:27 +0100

    change port

--- backend/index.ts
+++ backend/index.ts
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express(),
       bodyParser = require("body-parser"),
-      port = 3080;
+      port = 8080;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'frontend')));
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,9 +3,10 @@ const path = require("path");
 module.exports = {
     outputDir: path.resolve(__dirname, "../backend/dist/frontend"),
     devServer: {
+      port: 8081,
       proxy: {
         '^/api': {
-          target: 'http://localhost:3080',
+          target: 'http://localhost:8080',
           changeOrigin: true
         },
       }
