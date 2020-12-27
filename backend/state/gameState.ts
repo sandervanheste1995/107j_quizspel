8d6576d34b7448df4e5a37b544a922877e648cc7
Sander Vanheste <sandrvanheste@gmail.com>
2020-12-27 20:32:41 +0100

    init state object

--- backend/state/gameState.ts
+++ backend/state/gameState.ts
@@ -2,6 +2,14 @@ import config from '../config';
 import { GameState } from "../shared/GameState";
 const fs = require('fs');
 
+const defaultState = {
+    clientState: {
+        viewName: 'Home'
+    }
+};
+
 export const state: { 
     clientState: GameState
-} = JSON.parse(fs.readFileSync(config.gameStateSavefile));
+} = fs.existsSync(config.gameStateSavefile) 
+    ? JSON.parse(fs.readFileSync(config.gameStateSavefile))
+    : defaultState;
