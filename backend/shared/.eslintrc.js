f060bf148c7f8a6576877a22c09872615269a67b
Sander Vanheste <sandrvanheste@gmail.com>
2020-12-28 15:25:41 +0100

    add esconfig

---
+++ backend/shared/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    root: false,
+    env: {
+      node: true
+    },
+    'extends': [
+      'eslint:recommended'
+    ],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module"
+    },
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    }
+  }
+  
