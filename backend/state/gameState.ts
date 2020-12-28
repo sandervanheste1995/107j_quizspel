1d337d5842f3de062a505a826fc947357a623eff
Sander Vanheste <sandrvanheste@gmail.com>
2020-12-28 17:40:47 +0100

    add default state

--- backend/state/gameState.ts
+++ backend/state/gameState.ts
@@ -8,6 +8,19 @@ export interface ServerState {
     clientState: GameState;
 };
 
+const defaultState: ServerState = {
+    minigameIndex: -1,
+    minigameStack: [],
+    clientState: {
+        teams: [[],[],[]],
+        viewName: 'Home'
+    }
+};
+
+if(!fs.existsSync(config.defaultGameStateFile)) {
+    fs.writeFileSync(config.defaultGameStateFile, JSON.stringify(defaultState, null, 4) , 'utf-8');
+}
+
 export const state: ServerState = 
     fs.existsSync(config.gameStateSavefile) 
     ? JSON.parse(fs.readFileSync(config.gameStateSavefile))
